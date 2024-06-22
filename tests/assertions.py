74a8506738f97b03571f400d1f36b5fa90c43918
omriweinstein <omri.weinstein@gmail.com>
2024-06-22 17:45:52 +0300

    created all the tests in assn3_tests.py

---
+++ tests/assertions.py
@@ -0,0 +1,43 @@
+import string
+import sys
+import requests
+
+import connectionController
+books_id_array = []
+
+def assert_status_code(response: requests.Response, status_code: int):
+    assert response.status_code == status_code
+
+
+def assert_ret_value(response: requests.Response, field: str, returned_value: any):
+    assert response.json()[field] == returned_value
+
+def assert_valid_post_response(response1: requests.Response, response2: requests.Response, response3: requests.Response):
+    id1, id2, id3 = response1.json()['ID'], response2.json()['ID'], response3.json()['ID']
+    assert isinstance(id1, str) and isinstance(id2, str) and isinstance(id3, str)
+    assert id1 != id2 
+    assert id1 != id3
+    assert id2 != id3
+    books_id_array.append(id1)
+    books_id_array.append(id2)
+    books_id_array.append(id3)
+
+def assert_valid_added_resource(response: requests.Response):
+    assert response.status_code == 201
+    # should be positive ID
+    VALID_RETURNED_RESOURCE_ID = 0
+    print("print(response.json()) > 0? response.json() =")
+    print(response.json())
+    sys.stdout.flush()
+    assert response.json() > VALID_RETURNED_RESOURCE_ID
+
+
+def assert_not_existed_meal(meal_identifier: any) -> None:
+    response = connectionController.http_get(f"meals/{meal_identifier}")
+    assert_status_code(response, error_code=404)
+    assert_ret_value(response, returned_value=-5)
+
+def assert_length_of_array(response: requests.Response, length: int):
+    assert len(response.json()) == length
+
+
--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -1,65 +1,48 @@
-# The tests to execute
-# There are 7 tests to be executed in pytest against your image:
-# 1. Execute three POST /books requests supplying the details of book1, book2, and book3 (see slide “Books”). The test is successful* if (i) all 3
-# requests return unique IDs (none of the IDs are the same), and (ii) the
-# return status code from each POST request is 201.
-# 2. Execute a GET books/<-ID> request, using the ID of book1 Adventures of
-# Huckleberry Finn. The test is successful if (i) the authors field equals “Mark
-# Twain” and (ii) the return status code from the request is 200.
-# 3. Execute a GET /books request. The test is successful if (i) the returned JSON object has 3 embedded JSON objects (books), and (ii) the return
-# status code from the GET request is 200.
-# * “The test is successful” means that your pytest needs to test for these
-# conditions.
-# The tests to execute
-# 4. Execute a POST /books request supplying the details of book4. The test is
-# successful if the return status code is 500 (since Google Books API will
-# return an error because the ISBN number is not correct).
-# 5. Perform a DELETE /books request, using the ID of book2 The Best of Isaac
-# Asimov. The test is successful if the return status code is 200.
-# 6. Perform a GET books/<-ID> request, using the ID of book2. The test is
-# successful if the return status code is 404.
-# 7. Execute a POST /books request supplying the details of book5. The test is
-# successful if the return status code is or 422 (the genre is not an
-# acceptable one). 
-
-import requests
-import json
-import pytest
-import random
-import string
+from books import books_array
+import connectionController
+from assertions import *
+import symtable
 
 # Test 1
-def test_post_books():
-    # Book 1
-    book1 = {
-        "title": "Adventures of Huckleberry Finn",
-        "authors": "Mark Twain",
-        "publishedDate": "1884-12-10",
-        "ISBN": "9780199536559",
-        "genre": "Adventure"
-    }
-    # Book 2
-    book2 = {
-        "title": "The Best of Isaac Asimov",
-        "authors": "Isaac Asimov",
-        "publishedDate": "1973-01-01",
-        "ISBN": "9780385420785",
-        "genre": "Science Fiction"
-    }
-    # Book 3
-    book3 = {
-        "title": "The Great Gatsby",
-        "authors": "F. Scott Fitzgerald",
-        "publishedDate": "1925-04-10",
-        "ISBN": "9780743273565",
-        "genre": "Fiction"
-    }
-
+def test_post_books():  
     # POST /books request for book1
-    response1 = requests.post("http://localhost:5001/books", json=book1)
-    
-    # POST /books request for book2
-    response2 = requests.post("http://localhost:5001/books", json=book2)
+    response1 = connectionController.http_post("books", books_array[0])
+    assert_status_code(response1, 201)
+    response2 = connectionController.http_post("books", books_array[1])
+    assert_status_code(response2, 201)
+    response3 = connectionController.http_post("books", books_array[2])
+    assert_status_code(response3, 201)
+    assert_valid_post_response(response1, response2, response3)
 
-    # 
-    response3 = requests.post("http://localhost:5001/books", json=book3)
+# Test 2
+def test_get_book_by_id():
+    # add id to string in the response
+    response = connectionController.http_get(f"books/{books_id_array[0]}")
+    assert_status_code(response, 200)
+    assert_ret_value(response, "authors", "Mark Twain")
+
+# Test 3
+def test_get_books():
+    response = connectionController.http_get("books")
+    assert_status_code(response, 200)
+    assert_length_of_array(response, 3)
+
+# Test 4 
+def test_post_books_with_invalid_ISBN():
+    response = connectionController.http_post("books", books_array[3])
+    assert_status_code(response, 500)
+    
+# Test 5
+def test_delete_books():
+    response = connectionController.http_delete(f"books/{books_id_array[1]}")
+    assert_status_code(response, 200)
+
+# Test 6
+def test_get_deleted_book():
+    response = connectionController.http_get(f"books/{books_id_array[1]}")
+    assert_status_code(response, 404)
+
+# Test 7
+def test_post_books_with_invalid_genre():
+    response = connectionController.http_post("books", books_array[4])
+    assert_status_code(response, 422)
